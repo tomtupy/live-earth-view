3a52c0ceb55a12c5b00cc9b8263d220955713a60
Tom Tupy <tomtupy@gmail.com>
2021-01-23 14:44:29 -0800

    Create utils.py

---
+++ lib/utils.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+import glob
+import os
+
+
+def compute_list_diff(list1, list2):
+    return [element for element in list1 if element not in list2]
+
+
+def get_timestamp_from_file_path(file_path):
+    return int(os.path.splitext(os.path.basename(file_path))[0])
+
+
+def get_image_file_list(data_path, image_format):
+    return sorted(
+        glob.glob(os.path.normpath(data_path) + '/*' + image_format)
+    )
+
+
+def get_image_file_path(path, filename):
+    return os.path.abspath(path) + '/' + filename
+
+
+def get_current_utc_timestamp(preceeding_minutes=0):
+    # Timestamp format:
+    # 4 digit year
+    # 3 digit day of year
+    # 2 digit hour
+    # 2 digit minute
+    # 2 digit second - <discard>
+    # 1 digit tenth of second - <discard>
+    dt_now = datetime.utcnow() - timedelta(minutes=preceeding_minutes)
+    return int(dt_now.strftime('%Y%j%H%M'))
+
+
+def parse_image_dimension_str(dim_str):
+    return [int(dim) for dim in dim_str.split('x')]
+
+
+def verify_image_dir_existence(dir_path):
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+    return os.path.exists(dir_path)
